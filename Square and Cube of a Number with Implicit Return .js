16d27c1c12377cc0243619be132c8313b84a2f8b
Jaschrome <65113665+Jaschrome@users.noreply.github.com>
2025-05-31 20:33:51 +0530

    Update Square and Cube of a Number with Implicit Return .js

--- Square and Cube of a Number with Implicit Return .js
+++ Square and Cube of a Number with Implicit Return .js
@@ -1,1 +1,9 @@
 const calculateSquareAndCube = (num) => ({ square: num * num, cube: num * num * num });
+const userInput = prompt("Enter a number:");
+const number = parseFloat(userInput);
+if (!isNaN(number)) {
+    const result = calculateSquareAndCube(number);
+    console.log(result);
+} else {
+    console.log("Invalid input. Please enter a valid number.");
+}
